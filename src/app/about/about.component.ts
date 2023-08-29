6479d560243c6ed3ad52a7330c1398338d4bc816
Paul Zijlmans <paulzijlmans@hotmail.com>
2023-08-29 09:18:57 +0200

    create custom observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Observable, noop } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const http$ = new Observable((observer) => {
+      fetch("/api/courses")
+        .then((response) => {
+          return response.json();
+        })
+        .then((body) => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
 
+    http$.subscribe(
+      (courses) => console.log(courses),
+      noop,
+      () => console.log("completed")
+    );
+  }
 }
