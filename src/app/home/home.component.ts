c44867a8cc45eebd7cefaccc98a3ea9ea7c86bf7
Paul Zijlmans <paulzijlmans@hotmail.com>
2023-09-05 09:25:37 +0200

    error handling - catch and replace

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+import { catchError, map, shareReplay, tap } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
 import { Course } from "../model/course";
 
@@ -16,8 +16,25 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     const http$ = createHttpObservable("api/courses");
     const courses$: Observable<Course[]> = http$.pipe(
+      tap(() => console.log("HTTP request executed")),
       map((result) => result["payload"]),
-      shareReplay()
+      shareReplay(),
+      catchError(() =>
+        of([
+          {
+            id: 0,
+            description: "RxJs In Practice Course",
+            iconUrl:
+              "https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png",
+            courseListIcon:
+              "https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png",
+            longDescription:
+              "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+            category: "BEGINNER",
+            lessonsCount: 10,
+          },
+        ])
+      )
     );
 
     this.beginnerCourses$ = courses$.pipe(
